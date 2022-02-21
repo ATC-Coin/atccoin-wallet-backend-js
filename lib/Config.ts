b8bd7990cd41fa2961e474fba2aef873687f395e
ATCCoin <92318834+ATC-Coin@users.noreply.github.com>
2022-02-21 18:42:53 +0000

    ATC implemented

--- lib/Config.ts
+++ lib/Config.ts
@@ -256,7 +256,7 @@ export class Config implements IConfig {
     /**
      * Your coins 'ticker', generally used to refer to the coin, i.e. 123 TRTL
      */
-    public ticker: string = 'TRTL';
+    public ticker: string = 'atsc';
 
     /**
      * Most people haven't mined any blocks, so lets not waste time scanning
@@ -298,14 +298,14 @@ export class Config implements IConfig {
     /**
      * The length of a standard address for your coin
      */
-    public standardAddressLength: number = 99;
+    public standardAddressLength: number = 98;
 
     /* The length of an integrated address for your coin - It's the same as
        a normal address, but there is a paymentID included in there - since
        payment ID's are 64 chars, and base58 encoding is done by encoding
        chunks of 8 chars at once into blocks of 11 chars, we can calculate
        this automatically */
-    public integratedAddressLength: number = 99 + ((64 * 11) / 8);
+    public integratedAddressLength: number = 98 + ((64 * 11) / 8);
 
     /**
      * A replacement function for the JS/C++ underivePublicKey.
